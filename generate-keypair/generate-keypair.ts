578dc5d66206b75781455c29468accf4be235f5c
AnikethDeb <techrunstheworld@gmail.com>
2024-08-03 13:30:45 +0530

    fix:generate-keyPair

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -1,6 +1,8 @@
-import "dotenv/config.js"
-import {getKeypairFromEnvironment} from "@solana-developers/helpers"
-const Keypair = getKeypairFromEnvironment("SECRET_KEY")
-    console.log(
-        `✅ Finished! We've loaded our secret key securely, using an env file!`
-      );
+import {Keypair} from  "@solana/web3.js"
+const keypair = Keypair.generate();
+console.log("the public key is ",keypair.publicKey.toBase58());
+console.log("the private key is ",keypair.secretKey);
+
+
+
+console.log("✅  done ");
